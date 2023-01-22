d7959734640378db8fb16e4878e3a5e0e91fea57
TallesC <tallescesar@yahoo.com.br>
2023-01-22 18:56:42 -0300

    amount of transactions in a specific month method using KISS and a God object anti-pattern

--- src/extratobancario/BankTransactionAnalyzerSimple.java
+++ src/extratobancario/BankTransactionAnalyzerSimple.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
@@ -19,8 +22,18 @@ public class BankTransactionAnalyzerSimple {
             final double amount = Double.parseDouble(colums[1]);
             total += amount;
         }
-
         System.out.println("The total for all transactions is " + total);
+
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        int cont = 0;
+        for (final String line : lines){
+            final String[] colums = line.split(",");
+            final LocalDate date = LocalDate.parse(colums[0], DATE_PATTERN);
+            if(date.getMonth() == Month.JANUARY){
+                cont++;
+            }
+        }
+        System.out.println("The number of transactions in this month is " + cont);
 
     }
 
